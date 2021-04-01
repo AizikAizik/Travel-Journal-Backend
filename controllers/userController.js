b6d3ac07884901fef11a8747230c8ddb4fb62536
AizikAizik <isaac.ogunleye@hotmail.com>
2021-04-01 07:27:32 -0700

    added registeration functionality with pre save hashing mongoose middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,45 +1,69 @@
-import asyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 import { generateToken } from "../utils/token.js";
 
 // GET /api/user
 // DESC controller for fetching all users in the database
 // private route for admin
-const fetchAllUsers = asyncHandler(
-    async (req, res) =>{
-        const users = await User.find({});
-        res.send(users);
-    }
-)
+const fetchAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.send(users);
+});
 
 // POST /api/user/login
 // DESC controller for authenticating user login
 // public route
-const authLogin = asyncHandler(
-    async (req, res) =>{
-        const {
-            email,
-            password
-        } = req.body;
+const authLogin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-        const user = await User.findOne({ email});
+  const user = await User.findOne({ email });
 
-        if(user && (await user.matchPassword(password))){
-            res.json({
-                id: user.id,
-                fullName: user.fullName,
-                email: user.email,
-                isAdmin: user.isAdmin,
-                token: generateToken(user.id)
-            })
-        }else{
-            res.status(401);
-            throw new Error("Invalid email or password");
-        }
-    }
-)
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      id: user.id,
+      fullName: user.fullName,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user.id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
 
-export {
-    fetchAllUsers,
-    authLogin,
-}
+// POST /api/user/register
+// DESC controller for registering a user
+// public route
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password, fullName } = req.body;
+
+  //first check if email exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    email,
+    password,
+    fullName,
+  });
+
+  if (user) {
+    res.status(201).json({
+      id: user.id,
+      fullName: user.fullName,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user.id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+export { fetchAllUsers, authLogin, registerUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { fetchAllUsers, authLogin } from "../controllers/userController.js";
-import {protect} from "../middlewares/authMiddleware.js";
+import {
+  fetchAllUsers,
+  authLogin,
+  registerUser,
+} from "../controllers/userController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get('/', protect, fetchAllUsers);
-router.post('/login', authLogin);
+router.get("/", protect, fetchAllUsers);
+router.post("/login", authLogin);
+router.post("/register", registerUser);
 
 export default router;
