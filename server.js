4e6b23d061cee14af4b214d60c17e12c0a5f3b9f
AizikAizik <isaac.ogunleye@hotmail.com>
2021-04-15 10:14:15 -0700

    added specific multiple cors origin

--- server.js
+++ server.js
@@ -18,11 +18,25 @@ connectDB();
 
 const app = express();
 
+const allowedDomains = [process.env.CORS_DOMAIN, 'http://localhost:3000'];
+
 // run utility middlewares
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors({origin : process.env.CORS_ORIGIN}));
 app.use(helmet());
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // bypass the requests with no origin (like curl requests, mobile apps, etc )
+    if (!origin) return callback(null, true);
+ 
+    if (allowedDomains.indexOf(origin) === -1) {
+      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //home route
 app.get('/', (req, res) => {
